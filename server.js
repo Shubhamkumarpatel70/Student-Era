141b3c838682dda4b919922a9afa2b7bfe871c60
Shubham Kumar <sp6122041@gmail.com>
2025-02-19 14:56:20 +0530

    Prepare for deployment on Render

--- server.js
+++ server.js
@@ -266,7 +266,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle the root URL request
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
