288bdebe5cf6743090149535ad9bedd6b11cd77b
Shubham Kumar <sp6122041@gmail.com>
2025-05-04 15:55:28 +0530

    Update static file serving to include the entire directory

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for all routes
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
+
+console.log(path.join(__dirname))
 
 // Serve the generated student ID JSON file
 app.get("/", (req, res) => {
